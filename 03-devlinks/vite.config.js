4ee75f9650d96cc7453d5e1cb1b30c3b702e99ec
manuela582279 <manuela.domingues@aluno.senai.br>
2026-02-12 11:30:04 -0300

    feat: reestruturar componente App e adicionar componente Perfil

--- 03-devlinks/vite.config.js
+++ 03-devlinks/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+   server:{
+    port:30000, //Muda a porta padrao vite 5173 para a padrao react 3000
+    open:true, //Abre o navegador automaticamente
+    host:true, //Permite que tenha acesso via IP na rede local
+  }
+  }
+);
