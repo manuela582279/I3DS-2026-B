b7b7d1acc6567d16d7db6cf636e688a864912cfa
manuela582279 <manuela.domingues@aluno.senai.br>
2026-02-19 11:18:59 -0300

    feat: adicionar estrutura inicial do aplicativo com logotipo, barra de pesquisa e rodapé

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,11 +1,26 @@
-import "./App.css"
+import "./App.css";
+
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
-}
+    <div id="App">
+      <img
+        className="logo"
+        src={logo}
+        alt="Logotipo da plataforma de streaming DEVFLIX em cores vermelha e preta, ideal para quem busca filmes e séries de alta qualidade."
+      />
 
-export default App
+      <div className="saerch">
+        <input type="text" placeholder="Pesquise por filmes e séries.." />
+        <img src={lupa} alt="botão para a pesquisa" />
+
+      </div>
+      <Rodape>Manuela Domingues</Rodape>
+    </div>
+  );
+};
+
+export default App;
---
+++ 04-devflix/src/components/Rodape/Rodape.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Rodape.module.css";
+
+const Rodape = ({ children }) => {
+  return (
+    <footer>
+      <p>
+        Feito com ❤️por <a href="https://github.com">{children}</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
